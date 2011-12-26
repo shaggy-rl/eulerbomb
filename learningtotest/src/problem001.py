e2003a214048c1065033711d45700e8f182261e5
T.J. Eckman <tj.eckman@gmail.com>
2011-12-26 12:21:24 -0500

    Added time checks to 1 and 2

--- learningtotest/src/problem001.py
+++ learningtotest/src/problem001.py
@@ -1,8 +1,12 @@
 from euler import filter
+from time import time
 
 class problem001():
     def __init__(self):
+        self.start = time()
         self.answer = filter(1000,[3,5])
-
+        self.stop = time()
+        if (self.stop - self.start > 60):
+            self.answer = "Too much time used on number 1: " + str(self.stop - self.start)
 if __name__ == '__main__':
     print problem001().answer
--- learningtotest/src/problem002.py
+++ learningtotest/src/problem002.py
@@ -1,8 +1,11 @@
 from euler import fib,even,listsum
-
+from time import time
 class problem002():
     def __init__(self):
+        self.start = time()
         self.answer = reduce(listsum,filter(even,fib(4000000)))
-
+        self.stop = time()
+        if (self.stop - self.start > 60):
+            self.answer = "Too much time used on number 2: " + str(self.stop - self.start)
 if __name__ == '__main__':
     print problem002().answer
